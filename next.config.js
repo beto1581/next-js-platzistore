6955e92a123974f75e88ba8857102896edc5b3e8
beto1581 <nelsoncortezrueda1993@gmail.com>
2022-09-16 22:50:18 -0600

    next PWA

--- next.config.js
+++ next.config.js
@@ -1,26 +1,13 @@
-module.exports = {
+const withPWA = require('next-pwa');
+module.export = withPWA({
+  pwa:{
+    dest: 'public',
+    register: true,
+    mode: 'production',
+    disable: false,
+  },
   reactStrictMode: true,
   images: {
-    domains: ['placeimg.com',
-      'api.lorem.space',
-      'vuzoon.com',
-      'www.complementosdelcafe.com',
-      'www.cronista.com',
-      'www.tecnologia-informatica.com',
-      'www.multiofertas.hn',
-      'www.walmart.com.hn',
-      'i.blogs.es',
-      'www.amazon.com',
-      'www.sony.com.hn',
-      '*.*',
-      'cdn.pixabay.com',
-      'www.libreriahuequito.com',
-      'thumbs.dreamstime.com',
-      'wow.zamimg.com',
-      'bnetcmsus-a.akamaihd.net',
-      ''
-    ],
-
+    domains: ['placeimg.com','api.lorem.space','vuzoon.com','www.complementosdelcafe.com','www.cronista.com','www.tecnologia-informatica.com','www.multiofertas.hn','www.walmart.com.hn','i.blogs.es','www.amazon.com','www.sony.com.hn', '*.*','cdn.pixabay.com','www.libreriahuequito.com','thumbs.dreamstime.com','wow.zamimg.com','bnetcmsus-a.akamaihd.net', ''],
   },
-
-}
+});
